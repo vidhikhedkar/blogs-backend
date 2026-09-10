e950576f163b6f13bfbef8cb87bfdc8adbd55764
vidhikbk <vidhikhedkar.kbk@gmail.com>
2026-09-10 11:02:51 +0530

    add urls

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ connectDB();
 
 // Dynamic Allowed Origins based on environment variables
 const allowedOrigins = [
-    'http://localhost:5173',"https://venture-boreholes-frontend.onrender.com",
-    process.env.CLIENT_URL 
+    'http://localhost:5173', "https://venture-boreholes-frontend.onrender.com", "https://venture-boreholes-frontend.vercel.app", "https://venture-boreholes-client.vercel.app", "https://www.ventureborehole.com",
+    process.env.CLIENT_URL
 ].filter(Boolean);
 
 app.use(helmet());
