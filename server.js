35d743adc7eabfda37aaff265ff92f6eb6df140d
vidhikhedkar <vidhikhedkar7@gmail.com>
2026-09-15 11:34:39 +0530

    add

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Dynamic Allowed Origins based on environment variables
 const allowedOrigins = [
-    'http://localhost:5173', "https://venture-boreholes-frontend.onrender.com",
+    'http://localhost:5173', "https://blogs-frontend-b8x4.onrender.com",
     process.env.CLIENT_URL
 ].filter(Boolean);
 
